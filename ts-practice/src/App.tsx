4df50039fb0b4c2e803c059897d6edd03f2a2f51
ys.training240202 <ys.training240202@gmail.com>
2025-09-19 11:33:05 +0900

    ディレクトリ構成の修正

--- ts-practice/src/App.tsx
+++ ts-practice/src/App.tsx
@@ -1,64 +1,12 @@
+import practice_1 from './practice/practice_1'
+
 function App() {
 
-  const language: string = "TypeScript";
-
-  const code: number | null = 200;
-
-  const direction: "北" | "東" | "南" | "西" = "東";
-
-  const isAvailable: boolean[] = [true, false, true];
-
-  const person: { name: string, age: number, isStudent: boolean } = {
-    name: "taichi",
-    age: 18,
-    isStudent: true,
-  }
-
-  let data: unknown;
-
-  if (typeof data === "number") {
-    console.log("This is a number");
-  } else if (typeof data === "string") {
-    console.log("This is a string");
-  } else if (typeof data === "boolean") {
-    console.log("This is a boolean");
-  } else {
-    console.log("Unknown type");
-  }
-
-  type Status = "pending" | "resolved" | "rejected";
-
-  type Process = {
-    id: number;
-    title: string;
-    status: Status;
-    manager: string;
-  }
-  interface Employee {
-    name: string;
-    department: "営業" | "開発" | "人事" | "経理";
-    position?: string;
-    salary: number;
-  }
-
-  type Country = {
-    readonly name: string;
-    readonly capital: string;
-    population: number;
-  }
-
-  let book: unknown;
-  book = {
-    title: "Cinderella",
-    publicationYear: 1985,
-    pages: 46,
-    available: true
-  };
-
-  (book as { title: string, publicationYear: number, pages: number, available: boolean }).available = false;
-
-  console.log(book);
-
+  return (
+    <>
+      {practice_1()}
+    </>
+  )
 }
 
 export default App
---
+++ ts-practice/src/practice/practice_1.tsx
@@ -0,0 +1,64 @@
+const practice_1  = () => {
+
+  const language: string = "TypeScript";
+
+  const code: number | null = 200;
+
+  const direction: "北" | "東" | "南" | "西" = "東";
+
+  const isAvailable: boolean[] = [true, false, true];
+
+  const person: { name: string, age: number, isStudent: boolean } = {
+    name: "taichi",
+    age: 18,
+    isStudent: true,
+  }
+
+  let data: unknown;
+
+  if (typeof data === "number") {
+    console.log("This is a number");
+  } else if (typeof data === "string") {
+    console.log("This is a string");
+  } else if (typeof data === "boolean") {
+    console.log("This is a boolean");
+  } else {
+    console.log("Unknown type");
+  }
+
+  type Status = "pending" | "resolved" | "rejected";
+
+  type Process = {
+    id: number;
+    title: string;
+    status: Status;
+    manager: string;
+  }
+  interface Employee {
+    name: string;
+    department: "営業" | "開発" | "人事" | "経理";
+    position?: string;
+    salary: number;
+  }
+
+  type Country = {
+    readonly name: string;
+    readonly capital: string;
+    population: number;
+  }
+
+  let book: unknown;
+  book = {
+    title: "Cinderella",
+    publicationYear: 1985,
+    pages: 46,
+    available: true
+  };
+
+  (book as { title: string, publicationYear: number, pages: number, available: boolean }).available = false;
+
+  console.log(book);
+
+}
+
+export default practice_1
